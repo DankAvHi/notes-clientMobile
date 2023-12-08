1bc9e3644cac1085745d063b63a828e91d6cbb9b
DankAvHi <thexcat86@gmail.com>
2023-12-08 21:15:38 +0300

    [mv] app to src/app folder; rm boilerplate

---
+++ src/app/App.styles.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const AppStyles = StyleSheet.create({
+    App:{
+        height:"100%",
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    heading:{
+        fontSize:64,
+    }
+})
---
+++ src/app/App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from "react";
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, } from "react-native";
+import { AppStyles as styles } from "./App.styles";
+
+
+export const App = () => {
+    const isDarkMode = useColorScheme() === "dark";
+
+    return (
+        <SafeAreaView>
+            <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+            <ScrollView contentContainerStyle={styles.App} contentInsetAdjustmentBehavior="automatic">
+                <Text style={styles.heading}>{`Hello World!`}</Text>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+
